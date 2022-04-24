3a5d7933c79abd6b4b21565cd03f774c64ad6ade
alannuari <alannuari372@gmail.com>
2022-04-24 20:08:53 +0800

    hapus tag script

--- index.php
+++ index.php
@@ -49,8 +49,6 @@ if (isset($_SESSION['error'])) {
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="./css/style.css">
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/script.js"></script>
 		<title>Test ICUBE by SIRCLO</title>
 	</head>
 	<body>
